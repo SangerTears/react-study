20a106174d0453c82d53cd11462b838d43a51f4a
tears <2268858530@qq.com>
2019-05-16 14:36:19 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import './view/index.css';
+
+import RouterIndex from './router/index'
+class App extends Component {
+  render() { 
+    return <div className="pageWrap">
+    </div>;
+  }
+}
+export default App;
